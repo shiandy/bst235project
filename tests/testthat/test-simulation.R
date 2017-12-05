2e8b99bc805c454058e7a9e5d2037ad3f182f810
Andy Shi <andyshi@g.harvard.edu>
2017-12-04 19:08:46 -0500

    add a basic test for the simulation

--- tests/testthat/test-simulation.R
+++ tests/testthat/test-simulation.R
@@ -1,5 +1,20 @@
 context("test-simulation.R")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# setup code
+betas <- c(1, 0, 5)
+x_simulator <- function(n) {
+    mvtnorm::rmvnorm(n, sigma = diag(2))
+}
+error_simulator <- function(n) { rnorm(n, sd = 0.5) }
+nsims <- 10
+n <- 100
+set.seed(1262)
+system.time({
+    sim_res <- link_viol_sim(nsims, betas, x_simulator, n,
+                             error_simulator = error_simulator)
+})
+
+test_that("link_viol_sim returns objects with correct dimensions", {
+    expect_equal(dim(sim_res$betas), c(nsims, length(betas)))
+    expect_equal(dim(sim_res$errors), c(nsims, 3))
 })
