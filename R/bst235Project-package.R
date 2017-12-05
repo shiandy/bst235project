d5b0dd2cbebfc5684ca311a0a0359516134b14f2
Andy Shi <andyshi@g.harvard.edu>
2017-12-05 15:30:38 -0500

    add documentation for the overall R package

---
+++ R/bst235Project-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' bst235Project: BIOSTAT 235 course project, Fall 2017.
+#'
+#' This package contains the code for the BIOSTAT 235 Fall 2017 course
+#' project, problem 5.
+#'
+#' @docType package
+#' @name bst235Project
+NULL
